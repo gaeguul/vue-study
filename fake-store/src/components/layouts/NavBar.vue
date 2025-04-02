18ac760f12ead6ae5a8f6eec7d093a7f313fedd8
gaeguul <jeeham0226@gmail.com>
2025-04-02 10:30:55 +0900

    feat: FakeStore 헤더, 네비게이션 바 구현

--- fake-store/src/components/layouts/NavBar.vue
+++ fake-store/src/components/layouts/NavBar.vue
@@ -1,7 +1,41 @@
 <template>
-  <div>This is NavBar</div>
+  <nav class="navbar navbar-expand-sm navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <a class="fa-solid fa-house"></a>
+        {{ config.title }}</a
+      >
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li
+            v-for="(menu, index) in config.menus"
+            :key="index"
+            class="nav-item"
+          >
+            <a class="nav-link" aria-current="page" :href="menu.url">
+              {{ menu.title }}</a
+            >
+          </li>
+        </ul>
+      </div>
+      <button class="btn d-none d-lg-block">로그아웃</button>
+    </div>
+  </nav>
 </template>
 
-<script setup></script>
+<script setup>
+import config from '@/config';
+</script>
 
 <style></style>

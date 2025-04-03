3249e214dea24ced604cf9db0650fa7af27457c7
gaeguul <jeeham0226@gmail.com>
2025-04-03 09:34:16 +0900

    0402: axios-test-app 프로젝트 구현

---
+++ 0402/axios-test-app/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Axios 테스트</h1>
+</template>
+
+<script setup>
+import axios from 'axios';
+
+const requestApi = () => {
+  const requestUrl = '/api/contacts';
+  axios.get(requestUrl).then((response) => {
+    console.log(response);
+  });
+};
+requestApi();
+</script>
